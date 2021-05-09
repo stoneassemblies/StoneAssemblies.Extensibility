e1c8ff4a7679056857db67b53d4bb5fcf556e78c
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2021-05-09 02:52:14 -0400

    Fix build =

--- src/StoneAssemblies.Extensibility/Services/Interfaces/IExtensionManager.cs
+++ src/StoneAssemblies.Extensibility/Services/Interfaces/IExtensionManager.cs
@@ -11,35 +11,27 @@ namespace StoneAssemblies.Extensibility.Services.Interfaces
     using System.Threading.Tasks;
 
     /// <summary>
-    /// The ExtensionManager interface.
+    ///     The ExtensionManager interface.
     /// </summary>
     public interface IExtensionManager
     {
         /// <summary>
-        /// Gets the extension assemblies.
+        ///     Gets the extension assemblies.
         /// </summary>
         /// <returns>
-        /// The <see cref="IEnumerable{Assembly}"/>.
+        ///     The <see cref="IEnumerable{Assembly}" />.
         /// </returns>
         IEnumerable<Assembly> GetExtensionAssemblies();
 
         /// <summary>
-        /// Loads the extensions from package ids.
+        ///     Loads the extensions from package ids.
         /// </summary>
         /// <param name="packageIds">
-        /// The package ids.
+        ///     The package ids.
         /// </param>
         /// <returns>
-        /// The <see cref="Task"/>.
+        ///     The <see cref="Task" />.
         /// </returns>
         Task LoadExtensionsAsync(List<string> packageIds);
-
-        /// <summary>
-        /// Loads extensions.
-        /// </summary>
-        /// <returns>
-        /// The <see cref="Task"/>.
-        /// </returns>
-        Task LoadExtensionsAsync();
     }
 }
