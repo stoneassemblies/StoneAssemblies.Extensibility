bf2b8a2612aff0806cc66dcc6841cf1e942f8c61
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2021-06-19 21:46:38 -0400

    Move all test into single collection and disable parallelization

--- src/StoneAssemblies.Extensibility.Tests/Services/ExtensionManagerFacts.cs
+++ src/StoneAssemblies.Extensibility.Tests/Services/ExtensionManagerFacts.cs
@@ -6,9 +6,7 @@
 
 namespace StoneAssemblies.Extensibility.Tests.Services
 {
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
     using System.Threading.Tasks;
 
     using Microsoft.Extensions.Configuration;
@@ -16,6 +14,7 @@ namespace StoneAssemblies.Extensibility.Tests.Services
 
     using Moq;
 
+    using StoneAssemblies.Extensibility.Extensions;
     using StoneAssemblies.Extensibility.Services;
     using StoneAssemblies.Extensibility.Services.Interfaces;
 
@@ -27,51 +26,40 @@ namespace StoneAssemblies.Extensibility.Tests.Services
     public class ExtensionManagerFacts
     {
         /// <summary>
-        /// The get extension assembly method.
+        ///     The the load extensions async tests.
         /// </summary>
-        public class The_GetExtensionAssemblies_Method
+        [CollectionDefinition(nameof(The_LoadExtensionsAsync_Method), DisableParallelization = true)]
+        public class The_LoadExtensionsAsync_Method
         {
             /// <summary>
-            ///     Enum The loaded extensions.
+            ///     The creates the extension manager.
             /// </summary>
-            /// <returns>
-            ///     The <see cref="Task" />.
-            /// </returns>
             [Fact]
-            public async Task Enum_The_Loaded_Extensions()
+            public void Creates_The_ExtensionManager()
             {
                 var configurationMock = new Mock<IConfiguration>();
                 var serviceCollection = new ServiceCollection();
 
-                IExtensionManager extensionManager = new ExtensionManager(
-                    serviceCollection,
+                var extensionManager = serviceCollection.AddExtensions(
                     configurationMock.Object,
                     new List<string>
                         {
-                            "../../../../../output/nuget-local/",
-                            "https://api.nuget.org/v3/index.json",
-                        });
-
-                await extensionManager.LoadExtensionsAsync(
+                            "StoneAssemblies.Extensibility.DemoPlugin"
+                        },
                     new List<string>
                         {
-                            "StoneAssemblies.Extensibility.DemoPlugin",
+                            "../../../../../output/nuget-local/",
+                            "https://api.nuget.org/v3/index.json"
                         });
 
-                Assert.Single(extensionManager.GetExtensionAssemblies());
+                Assert.NotNull(extensionManager);
             }
-        }
 
-        /// <summary>
-        /// The the load extensions async tests.
-        /// </summary>
-        public class The_LoadExtensionsAsync_Method
-        {
             /// <summary>
-            ///     The load extensions async.
+            /// Initializes the plugin registering services in service collection.
             /// </summary>
             /// <returns>
-            ///     The <see cref="Task" />.
+            /// The <see cref="Task"/>.
             /// </returns>
             [Fact]
             public async Task Initializes_The_Plugin_Registering_Services_In_ServiceCollection()
@@ -85,26 +73,49 @@ namespace StoneAssemblies.Extensibility.Tests.Services
                     new List<string>
                         {
                             "../../../../../output/nuget-local/",
-                            "https://api.nuget.org/v3/index.json",
+                            "https://api.nuget.org/v3/index.json"
                         });
 
                 await extensionManager.LoadExtensionsAsync(
                     new List<string>
                         {
-                            "StoneAssemblies.Extensibility.DemoPlugin",
+                            "StoneAssemblies.Extensibility.DemoPlugin"
                         });
 
                 Assert.NotEmpty(serviceCollection);
             }
 
             /// <summary>
-            /// Succeeds even if extension list is null.
+            ///     Initializes the plugin registering services in service collection from the configuration.
             /// </summary>
             /// <returns>
-            /// The <see cref="Task"/>.
+            ///     The <see cref="Task" />.
             /// </returns>
             [Fact]
-            public async Task Empty_Load_Succeeds_Without_Exception()
+            public async Task Initializes_The_Plugin_Registering_Services_In_ServiceCollection_From_The_Configuration()
+            {
+                var dictionary = new Dictionary<string, string>
+                                     {
+                                         { "Extensions:Sources:0", "../../../../../output/nuget-local/" },
+                                         { "Extensions:Sources:1", "https://api.nuget.org/v3/index.json" },
+                                         { "Extensions:Packages:0", "StoneAssemblies.Extensibility.DemoPlugin" }
+                                     };
+
+                var configuration = new ConfigurationBuilder().AddInMemoryCollection(dictionary).Build();
+                var serviceCollection = new ServiceCollection();
+                IExtensionManager extensionManager = new ExtensionManager(serviceCollection, configuration);
+
+                await extensionManager.LoadExtensionsAsync();
+            }
+
+            /// <summary>
+            ///     Loads extensions assemblies available via get extension assemblies method.
+            /// </summary>
+            /// <returns>
+            ///     The <see cref="Task" />.
+            /// </returns>
+            [Fact]
+            public async Task Loads_Extensions_Assemblies_Available_Via_GetExtensionAssemblies_Method()
             {
                 var configurationMock = new Mock<IConfiguration>();
                 var serviceCollection = new ServiceCollection();
@@ -115,31 +126,62 @@ namespace StoneAssemblies.Extensibility.Tests.Services
                     new List<string>
                         {
                             "../../../../../output/nuget-local/",
-                            "https://api.nuget.org/v3/index.json",
+                            "https://api.nuget.org/v3/index.json"
                         });
 
-                await extensionManager.LoadExtensionsAsync();
+                await extensionManager.LoadExtensionsAsync(
+                    new List<string>
+                        {
+                            "StoneAssemblies.Extensibility.DemoPlugin"
+                        });
+
+                Assert.Single(extensionManager.GetExtensionAssemblies());
             }
 
             /// <summary>
-            /// Loads extensions from configuration.
+            ///     Registers services in service collection.
+            /// </summary>
+            [Fact]
+            public void Registers_Services_In_ServiceCollection()
+            {
+                var configurationMock = new Mock<IConfiguration>();
+                var serviceCollection = new ServiceCollection();
+
+                serviceCollection.AddExtensions(
+                    configurationMock.Object,
+                    new List<string>
+                        {
+                            "StoneAssemblies.Extensibility.DemoPlugin"
+                        },
+                    new List<string>
+                        {
+                            "../../../../../output/nuget-local/",
+                            "https://api.nuget.org/v3/index.json"
+                        });
+
+                Assert.NotEmpty(serviceCollection);
+            }
+
+            /// <summary>
+            ///     Succeeds even if extension list is null.
             /// </summary>
             /// <returns>
-            /// The <see cref="Task"/>.
+            ///     The <see cref="Task" />.
             /// </returns>
             [Fact]
-            public async Task Initializes_The_Plugin_Registering_Services_In_ServiceCollection_From_The_Configuration()
+            public async Task Succeeds_Without_Exception_For_Empty_Load()
             {
-                var dictionary = new Dictionary<string, string>
-                                          {
-                                              { "Extensions:Sources:0", "../../../../../output/nuget-local/" },
-                                              { "Extensions:Sources:1", "https://api.nuget.org/v3/index.json" },
-                                              { "Extensions:Packages:0", "StoneAssemblies.Extensibility.DemoPlugin" },
-                                          };
-
-                var configuration = new ConfigurationBuilder().AddInMemoryCollection(dictionary).Build();
+                var configurationMock = new Mock<IConfiguration>();
                 var serviceCollection = new ServiceCollection();
-                IExtensionManager extensionManager = new ExtensionManager(serviceCollection, configuration);
+
+                IExtensionManager extensionManager = new ExtensionManager(
+                    serviceCollection,
+                    configurationMock.Object,
+                    new List<string>
+                        {
+                            "../../../../../output/nuget-local/",
+                            "https://api.nuget.org/v3/index.json"
+                        });
 
                 await extensionManager.LoadExtensionsAsync();
             }
