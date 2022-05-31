79f868c83a82991beeb37e86f38dade6abb38d5a
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2022-05-31 01:02:05 -0400

    Adds missing XML doc comments

--- src/StoneAssemblies.Extensibility.Tests/Extensions/ServiceCollectionExtensionsFacts.cs
+++ src/StoneAssemblies.Extensibility.Tests/Extensions/ServiceCollectionExtensionsFacts.cs
@@ -1,4 +1,9 @@
-﻿
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ServiceCollectionExtensionsFacts.cs" company="Stone Assemblies">
+// Copyright © 2021 - 2021 Stone Assemblies. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
 namespace StoneAssemblies.Extensibility.Tests.Extensions
 {
     using Microsoft.Extensions.Configuration;
@@ -6,10 +11,19 @@ namespace StoneAssemblies.Extensibility.Tests.Extensions
     using Moq;
     using Xunit;
 
+    /// <summary>
+    /// The service collection extensions facts.
+    /// </summary>
     public class ServiceCollectionExtensionsFacts
     {
+        /// <summary>
+        /// The add extensions method.
+        /// </summary>
         public class The_AddExtensions_Method
         {
+            /// <summary>
+            ///     Initializes the plugin registering services in service collection.
+            /// </summary>
             [Fact]
             public void Initializes_The_Plugin_Registering_Services_In_ServiceCollection()
             {
@@ -33,8 +47,11 @@ namespace StoneAssemblies.Extensibility.Tests.Extensions
                 Assert.Equal(2, serviceCollection.Count);
             }
 
+            /// <summary>
+            ///     Does not initialize the plugin without registering services in service collection.
+            /// </summary>
             [Fact]
-            public void Does_Not_Initialize_The_Plugin_Registering_Services_In_ServiceCollection()
+            public void Does_Not_Initialize_The_Plugin_Without_Registering_Services_In_ServiceCollection()
             {
                 var configurationMock = new Mock<IConfiguration>();
                 var serviceCollection = new ServiceCollection();
